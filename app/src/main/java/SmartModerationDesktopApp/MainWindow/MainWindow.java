de661f0eae5daa36943a902a6db195d556c86a2f
BigKahuna7385 <danielburger2@gmx.de>
2022-04-14 15:38:11 +0200

    added login quick guide in own JFrame

--- app/src/main/java/SmartModerationDesktopApp/MainWindow/MainWindow.java
+++ app/src/main/java/SmartModerationDesktopApp/MainWindow/MainWindow.java
@@ -5,6 +5,7 @@ import SmartModerationDesktopApp.Observer.ServerObserver;
 import SmartModerationDesktopApp.Server.Client;
 import SmartModerationDesktopApp.Utilities.LineDrawer;
 import SmartModerationDesktopApp.Server.LoginController;
+import SmartModerationDesktopApp.Tutorial.Tutorial;
 import java.awt.GraphicsEnvironment;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -13,13 +14,13 @@ import javax.swing.Icon;
 import org.json.simple.parser.ParseException;
 
 public class MainWindow extends javax.swing.JFrame implements ServerObserver {
-
+    
     private final LineDrawer lineDrawer;
     private final LoginController loginController;
     private final ModerationCardsController moderationCardsController;
     private static MainWindow mainWindow;
     private Client client;
-
+    
     public MainWindow() {
         initComponents();
         setExtendedState(MAXIMIZED_BOTH);
@@ -27,14 +28,14 @@ public class MainWindow extends javax.swing.JFrame implements ServerObserver {
         loginController = new LoginController();
         moderationCardsController = new ModerationCardsController();
     }
-
+    
     public static MainWindow getInstance() {
         if (mainWindow == null) {
             mainWindow = new MainWindow();
         }
         return mainWindow;
     }
-
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -42,9 +43,10 @@ public class MainWindow extends javax.swing.JFrame implements ServerObserver {
         QRCode = new javax.swing.JLabel();
         QRCodeLabel = new javax.swing.JLabel();
         saveButton = new javax.swing.JButton();
+        quickGuide = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setAlwaysOnTop(true);
+        setTitle("Smart Moderation DesktopApp");
         setAutoRequestFocus(false);
         setPreferredSize(new java.awt.Dimension(1920, 1080));
         setResizable(false);
@@ -65,32 +67,45 @@ public class MainWindow extends javax.swing.JFrame implements ServerObserver {
             }
         });
 
+        quickGuide.setLabel("Login Quick Guide");
+        quickGuide.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                quickGuideActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(760, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
-                    .addComponent(QRCodeLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(QRCode, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(760, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(saveButton, javax.swing.GroupLayout.PREFERRED_SIZE, 125, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(20, 20, 20))
+                .addContainerGap(775, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(saveButton, javax.swing.GroupLayout.PREFERRED_SIZE, 125, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(20, 20, 20))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
+                            .addComponent(QRCode, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(QRCodeLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(quickGuide))
+                        .addGap(751, 751, 751))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(20, 20, 20)
                 .addComponent(saveButton, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 200, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 90, Short.MAX_VALUE)
                 .addComponent(QRCodeLabel)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(QRCode, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(215, Short.MAX_VALUE))
+                .addGap(30, 30, 30)
+                .addComponent(quickGuide)
+                .addContainerGap(219, Short.MAX_VALUE))
         );
+
+        quickGuide.getAccessibleContext().setAccessibleDescription("");
 
         pack();
         setLocationRelativeTo(null);
@@ -100,18 +115,23 @@ public class MainWindow extends javax.swing.JFrame implements ServerObserver {
         moderationCardsController.saveMeetingStatus();
     }//GEN-LAST:event_saveButtonActionPerformed
 
+    private void quickGuideActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_quickGuideActionPerformed
+        Tutorial tutorial = Tutorial.getInstance();
+        tutorial.setVisible(true);
+    }//GEN-LAST:event_quickGuideActionPerformed
+    
     private void initializeModerationCards() throws IOException {
         moderationCardsController.initializeModerationCards(client.getModerationCards());
         revalidate();
         repaint();
     }
-
+    
     private void readLoginInformation(String loginInformation) throws ParseException {
         loginController.readLoginInformation(loginInformation);
         setMeetingId(loginController.getMeetingId());
         client = new Client(loginController.getLoginInformation());
     }
-
+    
     @Override
     public void receiveLogin(String message) {
         try {
@@ -124,40 +144,40 @@ public class MainWindow extends javax.swing.JFrame implements ServerObserver {
             Logger.getLogger(MainWindow.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
+    
     @Override
     public void receivePutModerationCard(String message) {
         moderationCardsController.putModerationCard(message);
         revalidate();
         repaint();
     }
-
+    
     @Override
     public void receiveDeleteModerationCard(long cardId) {
         moderationCardsController.deleteModerationCard(cardId);
         revalidate();
         repaint();
     }
-
+    
     @Override
     public void receiveUpdateModerationCard(String message) {
         moderationCardsController.updateModerationCard(message);
         revalidate();
         repaint();
     }
-
+    
     public void setQRCodeLabel(Icon icon) {
         QRCode.setIcon(icon);
     }
-
+    
     public LineDrawer getLineDrawer() {
         return lineDrawer;
     }
-
+    
     public GraphicsEnvironment getGraphicsEnvironment() {
         return GraphicsEnvironment.getLocalGraphicsEnvironment();
     }
-
+    
     public void setMeetingId(long meetingId) {
         this.moderationCardsController.setMeetingId(meetingId);
     }
@@ -165,6 +185,7 @@ public class MainWindow extends javax.swing.JFrame implements ServerObserver {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel QRCode;
     private javax.swing.JLabel QRCodeLabel;
+    private javax.swing.JButton quickGuide;
     private javax.swing.JButton saveButton;
     // End of variables declaration//GEN-END:variables
 }
---
+++ app/src/main/java/SmartModerationDesktopApp/Tutorial/Tutorial.java
@@ -0,0 +1,62 @@
+package SmartModerationDesktopApp.Tutorial;
+
+import java.awt.GraphicsEnvironment;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+public class Tutorial extends javax.swing.JFrame {
+
+    private static Tutorial tutorial;
+
+    public Tutorial() {
+        initComponents();
+        Icon tutorial = new ImageIcon(getClass().getClassLoader().getResource("tutorial.gif"));
+        tutorialGif.setIcon(tutorial);        
+        centerFrame();
+    }
+
+    public static Tutorial getInstance() {
+        if (tutorial == null) {
+            tutorial = new Tutorial();
+        }
+        return tutorial;
+    }
+
+    private void centerFrame() {
+        int screenWidth = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().width;
+        int screenHeight = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().height;
+        setLocation(screenWidth / 2 - getBounds().width / 2, screenHeight / 2 - getBounds().height / 2);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        tutorialGif = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Login Quick Guide");
+        setAlwaysOnTop(true);
+        setPreferredSize(new java.awt.Dimension(350, 710));
+        setResizable(false);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(tutorialGif, javax.swing.GroupLayout.DEFAULT_SIZE, 342, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(tutorialGif, javax.swing.GroupLayout.DEFAULT_SIZE, 677, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel tutorialGif;
+    // End of variables declaration//GEN-END:variables
+}
